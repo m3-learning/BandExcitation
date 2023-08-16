2d2b3ef66d26dafe44d41cc7b4a554b2a2af3fe6
Josh Agar <jca318@lehigh.edu>
2023-08-16 16:56:11 -0400

    josh updates

--- BEWaveform.py
+++ BEWaveform.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.special import erf
 import matplotlib.pyplot as plt
+import warnings
 
 
 class AWG:
@@ -185,50 +186,107 @@ class BE_Viz:
         plt.grid(True)
         plt.show()
 
+def add_kwargs(obj, check = True, **kwargs):
+    for key, value in kwargs.items():
+        if not hasattr(obj, key) and check == True:
+            warnings.warn(f"Attribute '{key}' does not exist in the object. Setting it now.")
+        setattr(obj, key, value)
+        
+def inherit_attributes(source_obj, target_obj):
+    for key, value in source_obj.__dict__.items():
+        setattr(target_obj, key, value)
+        
+def phase_shift_waveform(waveform, shift_radians):
+    # get length of waveform
+    n = len(waveform)
+    
+    # Convert the phase shift from radians to points
+    shift_points = int(shift_radians / (2 * np.pi) * n) % n
+    
+    print(shift_points)
+
+    # Apply the phase shift
+    return np.roll(waveform, shift_points)
+
 class Spectroscopy():
     def __init__(
         self,
-        BE_ppw,
-        n_read = 256,
-        SS_steps_per_cycle= 64,
-               
+        **kwargs
     ) -> None:
         
+        add_kwargs(self, check=False, **kwargs)
         super().__init__()
-        self.BE_ppw = 2**BE_ppw
-        self.n_read = n_read
-        self.SS_steps_per_cycle = SS_steps_per_cycle
-        self.build_SS()
+        
+        # self.BE_ppw = 2**BE_ppw
+        # self.n_read = n_read
+        # self.SS_steps_per_cycle = SS_steps_per_cycle
+        # self.build_SS()
+        
+    def build_DC_wave(self):
+        if self.type == "switching spectroscopy":
+            self.switching_spectroscopy()
+        else:
+            raise ValueError("Invalid spectroscopy type")
 
-    def build_SS(self):
+    def switching_spectroscopy(self, **kwargs):
+        
+        add_kwargs(self, **kwargs)
+        
+        n = self.points_per_cycle + 1
+
+        # Lengths for each segment within the cycle
+        length_up = (self.max - self.start)
+        length_down = (self.max - self.min)
+        length_return = (self.start - self.min)
+
+        # Points for each segment within the cycle
+        x_up = np.linspace(0, length_up, int(n * length_up / (length_up + length_down + length_return)))
+        y_up = self.start + x_up
+
+        x_down = np.linspace(0, length_down, int(n * length_down / (length_up + length_down + length_return)))
+        y_down = self.max - x_down
+
+        x_return = np.linspace(0, length_return, int(n * length_return / (length_up + length_down + length_return)))
+        y_return = self.min + x_return
+
+        # Concatenate the points
+        x_cycle = np.concatenate([x_up, x_up[-1] + x_down, x_up[-1] + x_down[-1] + x_return])
+        y_cycle = np.concatenate([y_up, y_down, y_return])[:-1]
+        
+        if self.phase_shift is not None:
+            y_cycle = phase_shift_waveform(y_cycle, self.phase_shift)
+
+        # Repeat the cycle for cycles
+        self.DC_waveform = np.tile(y_cycle, self.cycles)
+
             
-            n_cycle = (2*self.n_read)  * self.SS_steps_per_cycle
-            interp_factor = (2*self.BE_ppw )/ self.n_read
-            n_step = 2*self.n_read 
-            n_write_vec = np.arange(self.n_read, n_cycle // 4, n_step)
+            # n_cycle = (2*self.n_read)  * self.SS_steps_per_cycle
+            # interp_factor = (2*self.BE_ppw )/ self.n_read
+            # n_step = 2*self.n_read 
+            # n_write_vec = np.arange(self.n_read, n_cycle // 4, n_step)
 
-            dc_amp_vec_1 = np.arange(0, self.SS_steps_per_cycle/4, 1)
-            dc_amp_vec_2 = np.arange(self.SS_steps_per_cycle/4, -1,-1)
+            # dc_amp_vec_1 = np.arange(0, self.SS_steps_per_cycle/4, 1)
+            # dc_amp_vec_2 = np.arange(self.SS_steps_per_cycle/4, -1,-1)
             
-            y_positive,y_negative = np.zeros(n_cycle // 4 - 1),np.zeros(n_cycle // 4 - 1)
+            # y_positive,y_negative = np.zeros(n_cycle // 4 - 1),np.zeros(n_cycle // 4 - 1)
             
-            for step_count in range(int(self.SS_steps_per_cycle/4)):
-                n_sub_shifted = np.arange(1, n_cycle // 4) - n_write_vec[step_count]
-                yk = 0.5 * (erf(n_sub_shifted ) - erf((n_sub_shifted - self.n_read)))
-                y_positive += dc_amp_vec_1[step_count] * yk
-                y_negative += dc_amp_vec_2[step_count] * yk       
-            n = np.arange(n_cycle - 4)
-            self.SS_wave  = np.interp(np.arange(1, int(n_cycle * interp_factor) + 1) / interp_factor, n, np.concatenate((y_positive,y_negative, -y_positive, -y_negative)))
+            # for step_count in range(int(self.SS_steps_per_cycle/4)):
+            #     n_sub_shifted = np.arange(1, n_cycle // 4) - n_write_vec[step_count]
+            #     yk = 0.5 * (erf(n_sub_shifted ) - erf((n_sub_shifted - self.n_read)))
+            #     y_positive += dc_amp_vec_1[step_count] * yk
+            #     y_negative += dc_amp_vec_2[step_count] * yk       
+            # n = np.arange(n_cycle - 4)
+            # self.SS_wave  = np.interp(np.arange(1, int(n_cycle * interp_factor) + 1) / interp_factor, n, np.concatenate((y_positive,y_negative, -y_positive, -y_negative)))
 
-            # getting the points where BE will be placed
-            n_write_vec = np.concatenate((np.arange(self.n_read, n_cycle // 2, n_step),np.arange(self.n_read, n_cycle // 2 - self.n_read - 1, n_step) + n_cycle // 2))
-            n_read_vec = np.concatenate(([1 / interp_factor], n_write_vec + (self.n_read )))
-            ni_read_vec = ((self.SS_steps_per_cycle*100)+n_read_vec * interp_factor)  
-            ni_write_vec = ((self.SS_steps_per_cycle*100)+n_write_vec * interp_factor) 
+            # # getting the points where BE will be placed
+            # n_write_vec = np.concatenate((np.arange(self.n_read, n_cycle // 2, n_step),np.arange(self.n_read, n_cycle // 2 - self.n_read - 1, n_step) + n_cycle // 2))
+            # n_read_vec = np.concatenate(([1 / interp_factor], n_write_vec + (self.n_read )))
+            # ni_read_vec = ((self.SS_steps_per_cycle*100)+n_read_vec * interp_factor)  
+            # ni_write_vec = ((self.SS_steps_per_cycle*100)+n_write_vec * interp_factor) 
 
-            self.SS_read_vec = np.round(ni_read_vec + np.round(interp_factor / 2))
-            SS_write_vec = np.round(ni_write_vec + np.round( interp_factor / 2))
-            self.SS_write_vec = np.concatenate((SS_write_vec, [SS_write_vec[-1] + np.round(n_step * interp_factor)]))
+            # self.SS_read_vec = np.round(ni_read_vec + np.round(interp_factor / 2))
+            # SS_write_vec = np.round(ni_write_vec + np.round( interp_factor / 2))
+            # self.SS_write_vec = np.concatenate((SS_write_vec, [SS_write_vec[-1] + np.round(n_step * interp_factor)]))
             
 class BE_Spectroscopy(BEWaveform,Spectroscopy):
 
@@ -236,15 +294,23 @@ class BE_Spectroscopy(BEWaveform,Spectroscopy):
         self,
         BE_ppw,
         BE_rep,
+        type = "switching spectroscopy",
+        start = None, 
+        max = None,
+        min = None,
+        cycles = None,
+        points_per_cycle = None,
+        phase_shift = None,
         center_freq=500e3,
         bandwidth=60e3,
         wave="chirp",
-        spectroscopic_waveform = "Bipolar",
         platform="PXI-5412",
         waveform_time=4e-3,
         BE_smoothing=125,
         BE_phase_var=None,
         chirp_direction="up",
+        measurement_state = "on and off", 
+        measurement_state_offset = 0,
         
     ) -> None:
         
@@ -260,35 +326,90 @@ class BE_Spectroscopy(BEWaveform,Spectroscopy):
                     BE_phase_var,
                     chirp_direction,
                     )
-        self.spectroscopic_waveform = spectroscopic_waveform
-        Spectroscopy.__init__(self, BE_ppw)
-        self.build_spectroscopy_waveform = self.build_spectroscopy_waveform()
         
-    def build_spectroscopy_waveform(self):
-        if self.spectroscopic_waveform == "Bipolar":
-            # merging BE and SS waveforms
-            n_step = len(self.BE_wave)
-            self.SS_wave[:n_step] += self.BE_wave
-            for step_count in range(len(self.SS_read_vec)):
-                start_idx = int(self.SS_read_vec[step_count]) 
-                end_idx = start_idx + n_step - 1
-                self.SS_wave[start_idx:end_idx] += self.BE_wave[: n_step - 1]
+        self.measurement_state = measurement_state
+        self.measurement_state_offset = measurement_state_offset
+        
+        
+        # self.spectroscopic_waveform = spectroscopic_waveform
+        Spectroscopy_ = Spectroscopy(type=type, start=start, max=max, min=min, cycles=cycles, points_per_cycle=points_per_cycle, phase_shift=phase_shift)
+        
+        inherit_attributes(Spectroscopy_, self)
+        
+        self.build_DC_wave()
+        self.build_spectroscopy_waveform()
+        
+        self.merge_low_and_high_freq(self.DC_wave, self.BE_wave)
+        
+    @staticmethod
+    def insert_constant(arr, constant):
+        result = []
+        for i, item in enumerate(arr):
+            result.append(item)
+            if i % 2 == 1:
+                result.append(constant)
+        return result
+    
+    def merge_low_and_high_freq(self, DC_wave, AC_wave):
+        # Create a result array with enough space to accommodate all replications
+        result = np.zeros(len(DC_wave) * len(AC_wave))
+
+        # Replicate the entire array AC_wave at each DC value
+        for i, offset in enumerate(DC_wave):
+            start_idx = i * len(AC_wave)
+            end_idx = start_idx + len(AC_wave)
+            result[start_idx:end_idx] = AC_wave + offset
             
-            for step_count in range(len(self.SS_write_vec)):
-                start_idx = int(self.SS_write_vec[step_count]) 
-                end_idx = start_idx + n_step - 1
-                self.SS_wave[start_idx:end_idx] += self.BE_wave[: n_step - 1]
+        self.cantilever_excitation_waveform = result
+        
+            
+    def build_spectroscopy_waveform(self):
+    
+        if self.measurement_state == "on and off":
+            self.DC_wave = self.insert_constant(self.DC_waveform, self.measurement_state_offset)
         else:
-            raise ValueError("Invalid spectroscopic waveform type")
-        return(self.SS_wave)
+            self.DC_wave = self.DC_waveform
+        
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+        
+        # self.build_spectroscopy_waveform = self.build_spectroscopy_waveform()
+        
+    # def build_spectroscopy_waveform(self):
+    #     if self.spectroscopic_waveform == "Bipolar":
+    #         # merging BE and SS waveforms
+    #         n_step = len(self.BE_wave)
+    #         self.SS_wave[:n_step] += self.BE_wave
+    #         for step_count in range(len(self.SS_read_vec)):
+    #             start_idx = int(self.SS_read_vec[step_count]) 
+    #             end_idx = start_idx + n_step - 1
+    #             self.SS_wave[start_idx:end_idx] += self.BE_wave[: n_step - 1]
+            
+    #         for step_count in range(len(self.SS_write_vec)):
+    #             start_idx = int(self.SS_write_vec[step_count]) 
+    #             end_idx = start_idx + n_step - 1
+    #             self.SS_wave[start_idx:end_idx] += self.BE_wave[: n_step - 1]
+    #     else:
+    #         raise ValueError("Invalid spectroscopic waveform type")
+    #     return(self.SS_wave)
 
-out = BE_Spectroscopy(14, 1, 600e3,60e3, wave= "chirp", BE_smoothing=125)
-viz = BE_Viz(out)
+# out = BE_Spectroscopy(14, 1, 600e3,60e3, wave= "chirp", BE_smoothing=125)
+# viz = BE_Viz(out)
 
-viz.plot_fft()
+# viz.plot_fft()
 
-viz.plot_waveform()
-viz.plot_merged_waveform()
+# viz.plot_waveform()
+# viz.plot_merged_waveform()
 # class BEWaveform:
 #     def __init__(
 #         self,
